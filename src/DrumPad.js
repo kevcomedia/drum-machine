e356cdc2c6c98904c50c8006ecd8ccc85bbae9f0
Kev Comedia <kevcomedia@gmail.com>
2019-06-02 20:13:53 +0800

    Implement drumpad audio

--- src/DrumPad.js
+++ src/DrumPad.js
@@ -1,12 +1,20 @@
-import React, { createRef } from 'react';
+import React, { useEffect, createRef } from 'react';
 import PropTypes from 'prop-types';
 
 function DrumPad(props) {
   const audioRef = createRef();
 
+  const activate = () => {
+    playAudio(audioRef.current);
+  };
+
+  useKeyDown(props.label, activate);
+
   return (
     <>
-      <button className="drum-pad">{props.label}</button>
+      <button className="drum-pad" onClick={activate}>
+        {props.label}
+      </button>
       <audio
         className="clip"
         src={props.audioSrc}
@@ -22,5 +30,25 @@ DrumPad.propTypes = {
   audioSrc: PropTypes.string.isRequired,
   audioName: PropTypes.string.isRequired,
 };
+
+function playAudio(audioElem) {
+  audioElem.currentTime = 0;
+  audioElem.play();
+}
+
+function useKeyDown(key, callback) {
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.keyCode === key.charCodeAt()) {
+        callback();
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  });
+}
 
 export default DrumPad;
